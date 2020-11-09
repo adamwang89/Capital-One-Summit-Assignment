a8ae5e30a08e81bc258a4a2417ea24687a7a4c6f
Adam Wang <adamwang@andrew.cmu.edu>
2020-11-08 23:57:47 -0500

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const favicon = require('express-favicon');
+const cors = require("cors");
 const path = require('path');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('78b9d599c4f94f8fa3afb1a5458928d6');
 const port = process.env.PORT || 8080;
 const app = express();
+app.use(cors());
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
